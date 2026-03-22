d6c847656bf01371d775f3efbff7d788a01dae65
Harman Singh <bhanguharmansingh88@gmail.com>
2026-03-22 14:53:25 +0530

    Add files via upload

---
+++ FIne_Tuning.py
@@ -0,0 +1,93 @@
+   # Finetuning Using Google Gemma's Model
+
+import os
+import transformers
+import torch
+from datasets import load_dataset
+from trl import SFTTrainer
+from peft import LoraConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import BitsAndBytesConfig, GemmaTokenizer
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["HF_KEY"] = os.getenv('HF_KEY')
+
+model_id = "google/gemma-2b"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id, token=os.environ['HF_KEY'])
+model = AutoModelForCausalLM.from_pretrained(model_id,
+                                             quantization_config=bnb_config,
+                                             device_map={"":0},
+                                             token=os.environ['HF_KEY'])
+
+text = "Quote: Imagination is more,"
+device = "cuda:0"
+inputs = tokenizer(text, return_tensors="pt").to(device)
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+
+
+text = "Quote: Imagination is more"
+device = "cuda:0"
+inputs = tokenizer(text, return_tensors="pt").to(device)
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+
+
+os.environ["WANDB_DISABLED"] = "false"
+
+lora_config = LoraConfig(
+    r = 8,
+    target_modules = ["q_proj", "o_proj", "k_proj", "v_proj",
+                      "gate_proj", "up_proj", "down_proj"],
+    task_type = "CAUSAL_LM",
+)
+
+from datasets import load_dataset
+
+data = load_dataset("Abirate/english_quotes")
+data = data.map(lambda samples: tokenizer(samples["quote"]), batched=True)
+
+def formatting_func(example):
+    text = f"Quote: {example['quote'][0]}\nAuthor: {example['author'][0]}"
+    return [text]
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=data["train"],
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=4,
+        warmup_steps=2,
+        max_steps=100,
+        learning_rate=2e-4,
+        fp16=True,
+        logging_steps=1,
+        output_dir="outputs",
+        optim="paged_adamw_8bit"
+    ),
+    peft_config=lora_config,
+    formatting_func=formatting_func,
+)
+
+trainer.train()
+
+text = "Quote: A woman is like a tea bag;"
+device = "cuda:0"
+inputs = tokenizer(text, return_tensors="pt").to(device)
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+text = "Quote: Outside of a dog, a book is man's"
+device = "cuda:0"
+inputs = tokenizer(text, return_tensors="pt").to(device)
+
+outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
